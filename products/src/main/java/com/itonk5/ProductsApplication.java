4ece977e106b571ab140ad6d50da8f96e1d2d40e
Lasse Have Nielsen <lasse@havenielsen.dk>
2016-05-24 20:54:17 +0200

    Orders and products completed.

--- products/src/main/java/com/itonk5/ProductsApplication.java
+++ products/src/main/java/com/itonk5/ProductsApplication.java
@@ -23,6 +23,11 @@ public class ProductsApplication {
 		add(new Product((long) 1, "Topman Black Pants", 22.95, Category.Pants, "Top quality pants, made for you. The black color is an elegant choice", "http://images.askmen.com/fashion/apparel/1273784368_topman-black-pants_1.jpg"));
 		add(new Product((long) 2, "Topman Navy Pants", 22.95, Category.Pants, "Top quality pants, made for you. The navy color is an elegant choice", "http://g.nordstromimage.com/ImageGallery/store/product/Large/7/_12036607.jpg"));
 		add(new Product((long) 3, "Ralph Lauren Silk-Cotton Pant", 32.95, Category.Pants, "Ralph Laurens no. 1 pant. Made of silk cotton. Flat-front (no pleats)...Zipper fly,slide clasp with hidden button,belt loops,2 angled front pockets, 2 back pockets with button closure and coin pocket.. Not lined. Dry Clean.", "http://ecx.images-amazon.com/images/I/41U-mmtgqCL._SX342_.jpg"));
+		add(new Product((long) 4, "ASOS Shorts In Wool Mix Pinstripe Dark Grey", 15.95, Category.Short, "Soft-touch, wool-mix fabric", "http://images.asos-media.com/inv/media/8/2/8/8/5748828/darkgrey/image1xxl.jpg"));
+		add(new Product((long) 5, "Vans Old Skool Trainers", 40.00, Category.Shoes, "Lace up fastening", "http://images.asos-media.com/inv/media/3/1/1/0/3930113/black/image1xxl.jpg"));
+		add(new Product((long) 6, "New Balance 373 Trainers", 59.99, Category.Shoes, "Real leather overlays.", "http://images.asos-media.com/inv/media/4/2/3/4/5884324/green/image1xxl.jpg"));
+		add(new Product((long) 7, "ASOS Bomber Jacket With MA1 Pocket In Light Khaki", 38.95, Category.Jackets, "Lightweight smooth fabric", "http://images.asos-media.com/inv/media/1/2/6/2/6302621/grey/image1xxl.jpg"));
+
 	}};
 
 
